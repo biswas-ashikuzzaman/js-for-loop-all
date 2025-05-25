9c5b917186acbb5460f63fb19dfffbe9304b3214
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-05-26 00:28:07 +0600

    update

--- for.js
+++ for.js
@@ -1,4 +1,8 @@
 let data= [12,23,0,-78,-89,-66,45,0,0];
 for (let i=0; i<data.length; i++) {
-    document.write(data[i]);
+    if (data[i]>0){
+        document.write( "Number of positive data:" + data[i]);
+    }
+    // document.write(data[i] + "<br>");
+    console.log(data[i])
 }
